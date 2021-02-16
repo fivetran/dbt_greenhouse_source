d1f1ebcb30f7c2b4428c737df9b7818579b1ffca
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-02-16 15:59:21 -0800

    add department stg model

---
+++ macros/get_department_columns.sql
@@ -0,0 +1,14 @@
+{% macro get_department_columns() %}
+
+{% set columns = [
+    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
+    {"name": "external_id", "datatype": dbt_utils.type_string()},
+    {"name": "id", "datatype": dbt_utils.type_int()},
+    {"name": "name", "datatype": dbt_utils.type_string()},
+    {"name": "parent_id", "datatype": dbt_utils.type_int()}
+] %}
+
+{{ return(columns) }}
+
+{% endmacro %}
